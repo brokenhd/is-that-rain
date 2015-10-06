fb865daf10dced78fce149719f140c7499eaa74c
brokenhd <michaelastarks@gmail.com>
2015-10-05 20:02:30 -0700

    adding shooting

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	int lifeSpan = 1;
+	
+	void Start() {
+		Destroy (gameObject, lifeSpan);
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if(col.gameObject.name == "Floor") {
+			Destroy (gameObject);
+		}
+	}
+}
